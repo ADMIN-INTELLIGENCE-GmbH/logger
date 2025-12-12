edda9f61dc12be9743d3036ee0364e3b4e6e0dcd
Julian Billinger <julian.billinger@admin-box.de>
2025-12-12 07:32:13 +0100

    style: Remove unnecessary blank lines in routes file

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@ use App\Http\Controllers\UserController;
 use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/', function () {
     return redirect()->route('dashboard');
 });
