e287cda141bd112ccd7d3decc562fee1eacc3760
Julian Billinger <julian.billinger@admin-box.de>
2025-12-12 07:28:30 +0100

    fix: Update Vite manifest creation in CI to prevent test failures

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\UserController;
 use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/', function () {
     return redirect()->route('dashboard');
 });
